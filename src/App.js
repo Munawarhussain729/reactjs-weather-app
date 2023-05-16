be20366eb3daba55474968d3c1a0331ba9d6d163
Munawar Hussain <munawar@epochclan.com>
2023-05-16 13:33:25 +0500

    Functionality Complete

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import './App.css';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
@@ -33,6 +34,28 @@ function App() {
           <Route path='/forcast' element={<Forcast />} />
         </Routes>
       </Router>
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+>>>>>>> Initialize project using Create React App
     </div>
   );
 }
